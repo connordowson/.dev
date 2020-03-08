b8afa7595957aa050600ebb5e4b4b5f61582c43d
connordowson <connormwdowson@gmail.com>
2020-03-08 22:19:13 +0000

    Started to fix Spotify api call

--- functions/getTopTracks/getTopTracks.js
+++ functions/getTopTracks/getTopTracks.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 const btoa = require("btoa");
 
 const {
@@ -12,30 +12,48 @@ const API_ENDPOINT = "https://accounts.spotify.com/api/token";
 
 const tokens = btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`);
 
+const { URLSearchParams } = require("url");
+
+const params = new URLSearchParams();
+params.append("grant_type", "client_credentials");
+params.append("scope", "user-top-read");
+
+// const body = { grant_type: "client_credentials", scope: "user-top-read" };
+
+fetch(API_ENDPOINT, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Basic ${tokens}`
+  },
+  body: params
+})
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+exports.handler = async (event, context, callback) => {
+  await fetch(API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${tokens}`
+    },
+    body: params
+  })
+    .then(res => res.json())
+    .then(data => {
+      callback(null, {
+        statusCode: 200,
+        body: data.access_token
+      });
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
+
 // exports.handler = function(event, context, callback) {
 //   callback(null, {
 //     statusCode: 200,
 //     body: tokens
 //   });
 // };
-
-const body = { grant_type: "client_credentials" };
-
-exports.handler = async (event, context, callback) => {
-  await axios({
-    method: "post",
-    url: API_ENDPOINT,
-    data: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${tokens}`
-    }
-  })
-    .then(res => {
-      callback(null, {
-        statusCode: 200,
-        body: res.data.access_token
-      });
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};

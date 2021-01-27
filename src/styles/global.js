e51a9297ae94b254e1c2484f904340574afc40f1
Connor Dowson <connormwdowson@gmail.com>
2021-01-27 16:46:11 +0000

    Remove font-display: swap

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,7 +10,7 @@ const GlobalStyles = createGlobalStyle`
 @font-face {
   font-family: 'Poppins';
   font-style: normal;
-  font-display: swap;
+  font-display: block;
   font-weight: 700;
   src: url('/fonts/poppins-700-latin.woff2') format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
@@ -22,7 +22,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: 'Inter';
   font-style: normal;
   font-weight: 100 900;
-  font-display: swap;
+  font-display: block;
   src: url('/fonts/inter-var-latin.woff2') format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
     U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,

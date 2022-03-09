b5f435179d59f0d2ff7f46660613c590876f135e
Connor Dowson <connormwdowson@gmail.com>
2022-03-09 16:36:53 +0000

    fix fonts

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,7 +1,35 @@
 //  https://github.com/gatsbyjs/gatsby/blob/master/examples/using-vanilla-extract/src/components/ColorModeToggle.tsx
-import { globalStyle } from "@vanilla-extract/css";
+import { globalStyle, globalFontFace } from "@vanilla-extract/css";
 import { vars } from "../styles/theme.css";
 const { colors, fonts } = vars;
+
+globalFontFace("Poppins", {
+  src: 'local("Poppins")',
+  fontWeight: `700`,
+  fontDisplay: "block",
+  fontStyle: "normal",
+  unicodeRange: `U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+  U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+  U+FEFF, U+FFFD`,
+});
+globalFontFace("Inter", {
+  src: 'local("Poppins")',
+  fontWeight: `100 900`,
+  fontDisplay: "block",
+  fontStyle: "normal",
+  unicodeRange: `U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+  U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+  U+FEFF, U+FFFD`,
+});
+globalFontFace("Fira Code", {
+  src: 'local("Poppins")',
+  fontWeight: `300 700`,
+  fontDisplay: "block",
+  fontStyle: "normal",
+  unicodeRange: `U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+  U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+  U+FEFF, U+FFFD`,
+});
 
 globalStyle("*", {
   boxSizing: `border-box`,

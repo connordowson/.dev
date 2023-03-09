463521272840a461dbacae2ac774999e0619e2e6
Connor Dowson <connormwdowson@gmail.com>
2023-03-09 16:54:47 +0000

    Add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://connordowson.dev",
   markdown: {
     shikiConfig: {
       theme: {

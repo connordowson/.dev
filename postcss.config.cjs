076f6252c78d10bb62d2003fcdf5286ccc7fd435
Connor Dowson <connormwdowson@gmail.com>
2023-03-09 16:41:46 +0000

    Fix code block styling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("postcss-import-ext-glob"), require("postcss-import")],
+  plugins: [
+    require("postcss-import-ext-glob"),
+    require("postcss-import"),
+    require("postcss-lightningcss"),
+  ],
 };

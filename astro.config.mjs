7fe3460c70608be5b94b5a6edd79de2ad38f4550
Connor Dowson <connormwdowson@gmail.com>
2023-06-20 16:44:26 +0100

    Only preload assets on necessary pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import { tokenColors } from "./city-lights.json";
 import remarkCodeTitles from "remark-code-titles";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import { s } from "hastscript";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,17 +26,34 @@ export default defineConfig({
     },
     remarkPlugins: [remarkCodeTitles],
     rehypePlugins: [
+      rehypeSlug,
       [
         rehypeAutolinkHeadings,
-        [
-          {
-            behavior: "after",
-            content: {
-              type: "text",
-              value: "#",
-            },
+
+        {
+          behavior: "append",
+          properties: {
+            className: ["heading-anchor"],
           },
-        ],
+          content: (node) => [
+            s(
+              "svg",
+              {
+                class: "slug-svg",
+                viewBox: "0 0 16 16",
+                version: "1.1",
+                width: "1.5rem",
+                height: "1.5rem",
+              },
+              [
+                s("path", {
+                  fillRule: "evenodd",
+                  d: "M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z",
+                }),
+              ]
+            ),
+          ],
+        },
       ],
     ],
   },

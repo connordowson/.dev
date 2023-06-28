281d27e68f6d851c2e936105ff74a483b7e030c7
Connor Dowson <connormwdowson@gmail.com>
2023-06-28 22:16:45 +0100

    Use astro image component

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,14 +1,5 @@
 declare module 'astro:content' {
 	interface Render {
-		'.mdx': Promise<{
-			Content: import('astro').MarkdownInstance<{}>['Content'];
-			headings: import('astro').MarkdownHeading[];
-			remarkPluginFrontmatter: Record<string, any>;
-		}>;
-	}
-}
-declare module 'astro:content' {
-	interface Render {
 		'.md': Promise<{
 			Content: import('astro').MarkdownInstance<{}>['Content'];
 			headings: import('astro').MarkdownHeading[];
@@ -19,14 +10,43 @@ declare module 'astro:content' {
 
 declare module 'astro:content' {
 	export { z } from 'astro/zod';
-	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]];
+	export type CollectionEntry<C extends keyof AnyEntryMap> = AnyEntryMap[C][keyof AnyEntryMap[C]];
 
-	export const image: () => import('astro/zod').ZodObject<{
+	// TODO: Remove this when having this fallback is no longer relevant. 2.3? 3.0? - erika, 2023-04-04
+	/**
+	 * @deprecated
+	 * `astro:content` no longer provide `image()`.
+	 *
+	 * Please use it through `schema`, like such:
+	 * ```ts
+	 * import { defineCollection, z } from "astro:content";
+	 *
+	 * defineCollection({
+	 *   schema: ({ image }) =>
+	 *     z.object({
+	 *       image: image(),
+	 *     }),
+	 * });
+	 * ```
+	 */
+	export const image: never;
+
+	// This needs to be in sync with ImageMetadata
+	export type ImageFunction = () => import('astro/zod').ZodObject<{
 		src: import('astro/zod').ZodString;
 		width: import('astro/zod').ZodNumber;
 		height: import('astro/zod').ZodNumber;
-		format: import('astro/zod').ZodString;
+		format: import('astro/zod').ZodUnion<
+			[
+				import('astro/zod').ZodLiteral<'png'>,
+				import('astro/zod').ZodLiteral<'jpg'>,
+				import('astro/zod').ZodLiteral<'jpeg'>,
+				import('astro/zod').ZodLiteral<'tiff'>,
+				import('astro/zod').ZodLiteral<'webp'>,
+				import('astro/zod').ZodLiteral<'gif'>,
+				import('astro/zod').ZodLiteral<'svg'>
+			]
+		>;
 	}>;
 
 	type BaseSchemaWithoutEffects =
@@ -42,135 +62,223 @@ declare module 'astro:content' {
 		| BaseSchemaWithoutEffects
 		| import('astro/zod').ZodEffects<BaseSchemaWithoutEffects>;
 
-	type BaseCollectionConfig<S extends BaseSchema> = {
-		schema?: S;
-		slug?: (entry: {
-			id: CollectionEntry<keyof typeof entryMap>['id'];
-			defaultSlug: string;
-			collection: string;
-			body: string;
-			data: import('astro/zod').infer<S>;
-		}) => string | Promise<string>;
-	};
-	export function defineCollection<S extends BaseSchema>(
-		input: BaseCollectionConfig<S>
-	): BaseCollectionConfig<S>;
+	export type SchemaContext = { image: ImageFunction };
 
-	type EntryMapKeys = keyof typeof entryMap;
+	type DataCollectionConfig<S extends BaseSchema> = {
+		type: 'data';
+		schema?: S | ((context: SchemaContext) => S);
+	};
+
+	type ContentCollectionConfig<S extends BaseSchema> = {
+		type?: 'content';
+		schema?: S | ((context: SchemaContext) => S);
+	};
+
+	type CollectionConfig<S> = ContentCollectionConfig<S> | DataCollectionConfig<S>;
+
+	export function defineCollection<S extends BaseSchema>(
+		input: CollectionConfig<S>
+	): CollectionConfig<S>;
+
 	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
-	type ValidEntrySlug<C extends EntryMapKeys> = AllValuesOf<(typeof entryMap)[C]>['slug'];
+	type ValidContentEntrySlug<C extends keyof ContentEntryMap> = AllValuesOf<
+		ContentEntryMap[C]
+	>['slug'];
 
 	export function getEntryBySlug<
-		C extends keyof typeof entryMap,
-		E extends ValidEntrySlug<C> | (string & {})
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
 	>(
 		collection: C,
 		// Note that this has to accept a regular string too, for SSR
 		entrySlug: E
-	): E extends ValidEntrySlug<C>
+	): E extends ValidContentEntrySlug<C>
 		? Promise<CollectionEntry<C>>
 		: Promise<CollectionEntry<C> | undefined>;
-	export function getCollection<C extends keyof typeof entryMap, E extends CollectionEntry<C>>(
+
+	export function getDataEntryById<C extends keyof DataEntryMap, E extends keyof DataEntryMap[C]>(
+		collection: C,
+		entryId: E
+	): Promise<CollectionEntry<C>>;
+
+	export function getCollection<C extends keyof AnyEntryMap, E extends CollectionEntry<C>>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => entry is E
 	): Promise<E[]>;
-	export function getCollection<C extends keyof typeof entryMap>(
+	export function getCollection<C extends keyof AnyEntryMap>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => unknown
 	): Promise<CollectionEntry<C>[]>;
 
-	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
-		Required<ContentConfig['collections'][C]>['schema']
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(entry: {
+		collection: C;
+		slug: E;
+	}): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(entry: {
+		collection: C;
+		id: E;
+	}): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(
+		collection: C,
+		slug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(
+		collection: C,
+		id: E
+	): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	/** Resolve an array of entry references from the same collection */
+	export function getEntries<C extends keyof ContentEntryMap>(
+		entries: {
+			collection: C;
+			slug: ValidContentEntrySlug<C>;
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+	export function getEntries<C extends keyof DataEntryMap>(
+		entries: {
+			collection: C;
+			id: keyof DataEntryMap[C];
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+
+	export function reference<C extends keyof AnyEntryMap>(
+		collection: C
+	): import('astro/zod').ZodEffects<
+		import('astro/zod').ZodString,
+		C extends keyof ContentEntryMap
+			? {
+					collection: C;
+					slug: ValidContentEntrySlug<C>;
+			  }
+			: {
+					collection: C;
+					id: keyof DataEntryMap[C];
+			  }
+	>;
+	// Allow generic `string` to avoid excessive type errors in the config
+	// if `dev` is not running to update as you edit.
+	// Invalid collection names will be caught at build time.
+	export function reference<C extends string>(
+		collection: C
+	): import('astro/zod').ZodEffects<import('astro/zod').ZodString, never>;
+
+	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
+	type InferEntrySchema<C extends keyof AnyEntryMap> = import('astro/zod').infer<
+		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
 
-	const entryMap: {
+	type ContentEntryMap = {
 		"blog": {
 "my-top-10-favourite-albums-from-2022.md": {
-  id: "my-top-10-favourite-albums-from-2022.md",
-  slug: "my-top-10-favourite-albums-from-2022",
-  body: string,
-  collection: "blog",
+	id: "my-top-10-favourite-albums-from-2022.md";
+  slug: "my-top-10-favourite-albums-from-2022";
+  body: string;
+  collection: "blog";
   data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "use-a-vscode-syntax-highlighting-theme-in-astro.md": {
-  id: "use-a-vscode-syntax-highlighting-theme-in-astro.md",
-  slug: "use-a-vscode-syntax-highlighting-theme-in-astro",
-  body: string,
-  collection: "blog",
+	id: "use-a-vscode-syntax-highlighting-theme-in-astro.md";
+  slug: "use-a-vscode-syntax-highlighting-theme-in-astro";
+  body: string;
+  collection: "blog";
   data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "useful-webdev-links.md": {
-  id: "useful-webdev-links.md",
-  slug: "useful-webdev-links",
-  body: string,
-  collection: "blog",
+	id: "useful-webdev-links.md";
+  slug: "useful-webdev-links";
+  body: string;
+  collection: "blog";
   data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "view-draft-posts-whilst-running-gatsby-blog-in-development.md": {
-  id: "view-draft-posts-whilst-running-gatsby-blog-in-development.md",
-  slug: "view-draft-posts-whilst-running-gatsby-blog-in-development",
-  body: string,
-  collection: "blog",
+	id: "view-draft-posts-whilst-running-gatsby-blog-in-development.md";
+  slug: "view-draft-posts-whilst-running-gatsby-blog-in-development";
+  body: string;
+  collection: "blog";
   data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "view-styles.md": {
-  id: "view-styles.md",
-  slug: "view-styles",
-  body: string,
-  collection: "blog",
+	id: "view-styles.md";
+  slug: "view-styles";
+  body: string;
+  collection: "blog";
   data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-},
-"music-collection": {
-},
+} & { render(): Render[".md"] };
+};
 "playground": {
-"holo-pokemon-card.mdx": {
-  id: "holo-pokemon-card.mdx",
-  slug: "holo-pokemon-card",
-  body: string,
-  collection: "playground",
-  data: InferEntrySchema<"playground">
-} & { render(): Render[".mdx"] },
-},
+};
 "projects": {
 "calcio.md": {
-  id: "calcio.md",
-  slug: "calcio",
-  body: string,
-  collection: "projects",
+	id: "calcio.md";
+  slug: "calcio";
+  body: string;
+  collection: "projects";
   data: InferEntrySchema<"projects">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "dissertation.md": {
-  id: "dissertation.md",
-  slug: "dissertation",
-  body: string,
-  collection: "projects",
+	id: "dissertation.md";
+  slug: "dissertation";
+  body: string;
+  collection: "projects";
   data: InferEntrySchema<"projects">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "picturesquepints.md": {
-  id: "picturesquepints.md",
-  slug: "picturesquepints",
-  body: string,
-  collection: "projects",
+	id: "picturesquepints.md";
+  slug: "picturesquepints";
+  body: string;
+  collection: "projects";
   data: InferEntrySchema<"projects">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "portfolio.md": {
-  id: "portfolio.md",
-  slug: "portfolio",
-  body: string,
-  collection: "projects",
+	id: "portfolio.md";
+  slug: "portfolio";
+  body: string;
+  collection: "projects";
   data: InferEntrySchema<"projects">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "workout-tracker.md": {
-  id: "workout-tracker.md",
-  slug: "workout-tracker",
-  body: string,
-  collection: "projects",
+	id: "workout-tracker.md";
+  slug: "workout-tracker";
+  body: string;
+  collection: "projects";
   data: InferEntrySchema<"projects">
-} & { render(): Render[".md"] },
-},
+} & { render(): Render[".md"] };
+};
 
 	};
+
+	type DataEntryMap = {
+		"music-collection": {
+"discogs": {
+	id: "discogs";
+  collection: "music-collection";
+  data: any
+};
+};
+
+	};
+
+	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
 	type ContentConfig = typeof import("../src/content/config");
 }
